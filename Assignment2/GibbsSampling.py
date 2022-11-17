6879cad9e1878dc6edb699f0892aab88c87ca53e
Darakhsh1999 <darakhsh@gmail.com>
2022-11-17 11:47:43 +0100

    code no passes all assertions with no errors

--- Assignment2/GibbsSampling.py
+++ Assignment2/GibbsSampling.py
@@ -1,3 +1,4 @@
+import time
 import nltk
 import pandas as pd
 import numpy as np
@@ -30,24 +31,43 @@ class Gibbs():
         self.word_counts = np.sum(self.doc_word_counts, axis= 0) # number of occurences of each word across all documents, len = voc_len
         self.words_per_doc = np.sum(self.doc_word_counts, axis= 1) # number of words in each document, len = nmb_docs
         self.total_tokens = sum(self.word_counts) # total tokens after processing
-        self.Z = [np.random.randint(0, self.n_topics+1, s) for s in self.words_per_doc] # random topic for every word in every document
+        self.Z = [np.random.randint(0, self.n_topics, s) for s in self.words_per_doc] # random topic for every word in every document
+
 
         # Topic and Word counts
-        self.topic_freq = np.zeros((self.vocab_len, self.n_topics))
+        self.topic_freq = np.zeros((self.n_docs, self.n_topics))
         self.word_freq = np.zeros((self.vocab_len, self.n_topics))
-        self.tokens = self.InitializeTokens()
+        self.tokens = self.InitializeTokens() # same shape as Z, but word_index instead of topic
         self.InitializeFrequencies()
 
         self.SanityCheck()
 
-    def SanityCheck():
+    def SanityCheck(self):
         ''' Run assertions to check sanity and catch errors '''
-        pass
 
+        # Check tokenization of document was correct
+        for d in range(self.n_docs):
+            assert(len(self.tokens[d]) == self.words_per_doc[d])
+
+        # Check sizes of Z and tokens 
+        z_size = 0
+        w_size = 0
+        for d in range(self.n_docs):
+            z_size += len(self.Z[d])
+            w_size += len(self.tokens[d])
+        assert(z_size == w_size)
+        assert(z_size == self.total_tokens)
+
+        # Check word count sums
+        assert(sum(self.word_counts) == sum(self.words_per_doc))
+
+        # Check token mapping is correct
+        random_word_idx = self.tokens[0][1]
+        word = self.vocab[random_word_idx]
+        assert(random_word_idx == self.vocab_hash.get(word))
 
 
     def InitializeTokens(self):
-        print(self.vocab_hash.items())
         tokens = []
         for d in range(self.n_docs):
             d_tokens = []
@@ -62,16 +82,15 @@ class Gibbs():
         
         for d in range(self.n_docs):
             for j in range(self.words_per_doc[d]):
+
+                # topic frequency
                 topic_dj = self.Z[d][j]
                 self.topic_freq[d][topic_dj] += 1
 
-        # Row is topic, colum is word and value is number of words with topic k
-        for d in range(self.n_docs):
-            for j in range(self.words_per_doc[d]):
+                # word frequency
+                v_idx = self.tokens[d][j]
+                self.word_freq[v_idx][topic_dj] += 1
 
-                v_idx = self.vocab_hash.get(self.tokens[d][j])
-                topic_dj = self.Z[d][j]
-                self.word_freq = np.array([[self.word_counts[i]] + [0]*(self.n_topics - 1) for i in range(self.vocab_len)]) # shape = (vocab_len x nmb_topics)
 
     def Run(self, iterations):
         
@@ -83,16 +102,15 @@ class Gibbs():
 
 
     def UpdateZ(self, d_idx, j_idx):
-        # Here we assume fixed d,j
-        
         """function to update topic for Z[d_idx][j_idx] according to collapsed Gibbs sampling"""
+
         old_topic = self.Z[d_idx][j_idx]
 
         w_idx = self.tokens[d_idx][j_idx]
 
-        P = self.calc_p(d_idx, j_idx, w_idx)
+        P = self.CalculateP(d_idx, j_idx, w_idx)
 
-        new_topic = self.sample(P)
+        new_topic = self.Sample(P)
 
         if new_topic != old_topic:
             self.topic_freq[d_idx][old_topic]-=1
@@ -104,23 +122,24 @@ class Gibbs():
             self.Z[d_idx][j_idx] = new_topic
 
 
-    def calc_p(self,d_idx, j_idx, w_idx):
+    def CalculateP(self,d_idx, j_idx, w_idx):
         """Helper function to calculate P vector"""
+
         curr_val = self.Z[d_idx][j_idx]
 
         n_d = self.topic_freq[d_idx]
         
         m_v = self.word_freq[w_idx]
 
-        m = sum(self.word_freq)
+        m = sum(self.word_freq) # maybe optimze
 
-        Q = [0]*self.n_topics
+        Q = [0]*self.n_topics # use numpy array instead
 
         for idx in range(self.n_topics):
             if idx != curr_val:
-                Q[idx] = self.calc_q(n_d[idx], m_v[idx], m[idx])
+                Q[idx] = self.CalculateQ(n_d[idx], m_v[idx], m[idx])
             else: 
-                Q[idx] = self.calc_q(n_d[idx]-1, m_v[idx]-1, m[idx]-1)
+                Q[idx] = self.CalculateQ(n_d[idx]-1, m_v[idx]-1, m[idx]-1)
 
         q_sum = sum(Q)
 
@@ -128,31 +147,28 @@ class Gibbs():
         return P
         
         
-    def calc_q(self, n, m_v, m):
+    def CalculateQ(self, n, m_v, m):
         """Helper function to calculate Q vector"""
         return ((self.alpha + n)*(self.beta + m_v))/(self.beta * self.vocab_len + m)
 
 
-    def sample(self,P):
+    def Sample(self, P):
         """Helper function to sample new Z_dj topic from given probability distibution P"""
         r = np.random.rand()
         s = 0
-        for idx in range(len(P)):
+        for idx in range(self.n_topics):
             s+= P[idx]
             if r < s:
                 return idx
     
 
-
-
-
     def simple_eval(self, topic, most_common_x):
         freqs = Counter()
 
-        for d_idx in range(self.nmb_of_docs):
+        for d_idx in range(self.n_docs):
                 for j_idx in range(len(self.Z[d_idx])):
                     if self.Z[d_idx][j_idx] == topic:
-                        w_idx = self.get_w_idx(d_idx, j_idx)
+                        w_idx = self.tokens[d_idx][j_idx]
                         word = self.vocab[w_idx]
                         freqs[word] += 1
 
@@ -161,10 +177,14 @@ class Gibbs():
 
 
 if __name__ == '__main__':
+    t_start = time.time()
     n_docs = 1550 
-    n_docs = 200 
     data = LoadData(n_docs)
     gibbs = Gibbs(data, n_docs, 10, 10, 0.01, 0.01)
-    for d in range(gibbs.n_docs):
-        print(d)
-        assert(len(gibbs.tokens[d]) == gibbs.words_per_doc[d])
+    t_end = time.time()
+    t_tot = t_end - t_start
+    print("Load up time", t_tot)
+    t_before = time.time()
+    gibbs.Run(1)
+    t_after = time.time()
+    print("One iterations took", t_after - t_before)
